7ee2cd6e5b20400fd1fdebf6c8ad826b9c30518a
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2023-12-30 19:47:40 -0800

    3.4 Creating the tread pool and storing threads

--- hello/src/lib.rs
+++ hello/src/lib.rs
@@ -1,7 +1,22 @@
-pub struct ThreadPool;
+use std::thread;
+pub struct ThreadPool {
+    threads: Vec<thread::JoinHandle<()>>,
+}
 
 impl ThreadPool{
-    pub fn new(size:u32) -> ThreadPool { ThreadPool }
+    pub fn new(size:usize) -> ThreadPool { 
+        assert!(size > 0);
+
+        let mut threads = Vec::with_capacity(size);
+        
+        for _ in 0..size{
+
+        }
+
+        ThreadPool {
+            threads
+        }
+    }
     pub fn execute<F>(&self, f: F)
         where F: FnOnce() + Send + 'static {}
 
