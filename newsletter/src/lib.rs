505aaac828ff232c7127f9fb15b9c271533d16cf
Jonel Cordero <jonelcordero@JonelsMacStudio.lan>
2024-01-01 20:44:43 -0800

    3.5 add integration test random port

--- newsletter/src/lib.rs
+++ newsletter/src/lib.rs
@@ -1,24 +1,18 @@
-use actix_web::{web, App, HttpRequest, HttpServer, HttpResponse, Responder};
+use actix_web::{web, App, HttpServer, HttpResponse};
+use actix_web::dev::Server;
+use std::net::TcpListener;
 
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
-}
-
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
 }
 
--- newsletter/src/main.rs
+++ newsletter/src/main.rs
@@ -1,7 +1,10 @@
 use newsletter::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind address");
+    run(listener)?.await
 }
 
